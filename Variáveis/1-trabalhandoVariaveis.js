311237e708744d688174ca75d31c60b38bd6f2be
antonioneto30 <antonioneto3000@gmail.com>
2023-06-14 20:58:16 -0300

    wip

---
+++ Variáveis/1-trabalhandoVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com Variáveis")
+
+
+//boas práticas declarar qual o tipo de variável
+
+//Variáveis declaradas com const mantêm valores constantes. 
+//const não pode ser atualizado nem declarado novamente.
+const idade = 23;
+const nome = "Antonio"
+
+//let pode ser atualizado, mas não declarado novamente
+let ano = 2020;
---
+++ Variáveis/2-atribuicaoVariaveis.js
@@ -0,0 +1,25 @@
+console.log("Trabalhando com atribuição de variáveis")
+
+
+//varável: qualquer coisa guardada na memória, pode mudar
+//não muda
+//Boas práticas usar const
+
+const idade = 23;
+let nome = "Antonio"; //se usar const ao invés de let ele não funciona
+let sobrenome = "Neto"; //se usar const ao invés de let ele não funciona
+
+console.log(nome,sobrenome);
+console.log(nome + " " + sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`)
+
+nome = nome + sobrenome;
+console.log(nome);
+
+let contador = 0
+contador = contador + 1
+
+let numero; //declarando variável
+numero = 5; //atribuindo valor
+numero = numero+1;
+console.log(numero)
