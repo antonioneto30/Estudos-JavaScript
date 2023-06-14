e8bde2095055b5c41c7522761edeb08574ac4dab
antonioneto30 <antonioneto3000@gmail.com>
2023-06-14 19:17:32 -0300

    wip

---
+++ Operadores/1-firstProgram.js
@@ -0,0 +1,13 @@
+console.log("asldkjas");
+
+const idade = 23
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2)
+console.log(idade/2)
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
+
